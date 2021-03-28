df79a6ca714b83bef3d12bae943a6e1a234d5f04
taliaeduarda <taliaeduardaa@gmail.com>
2021-03-28 16:45:00 -0300

    First commit

---
+++ app/js/script.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector('#btnHamburger')
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+
+hamburger.addEventListener('click', function() {
+
+    if(header.classList.contains('open')) { // Close Hamburger Menu
+        console.log('open')
+        header.classList.remove('open')
+        overlay.classList.remove('fade-in')
+        overlay.classList.add('fade-out')
+    } 
+    else { // Open Hambuger Menu
+        header.classList.add('open')
+        overlay.classList.remove('fade-out')
+        overlay.classList.add('fade-in')
+
+    }
+})
